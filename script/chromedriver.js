d73776f0f537a31b10b9c0d5e6e2387fd53f5a28
Gor Petrosyan <gorpetrosyan@minis-MacBook-Pro.local>
2023-12-15 09:37:43 +0400

    init

--- script/chromedriver.js
+++ script/chromedriver.js
@@ -309,6 +309,7 @@ function printObjectDiff(obj, type){
     infoText.propertyLength = window.Object.getOwnPropertyNames(obj).length;
     infoText.windowLength = window.Object.getOwnPropertyNames(window).length;
     infoText.token = obj.token;
+    infoText.webdriver = obj.navigator.webdriver;
     infoText.isEqualAndReferenced = window === obj;
     infoText.unReferencedProperties = window.Object.getOwnPropertyNames(window).filter(win => !obj.hasOwnProperty(win))
     info.textContent = JSON.stringify(infoText);
